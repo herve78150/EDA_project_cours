3b401bd013a38c51d9f170a317ec5d562d1794aa
herve78150 <herve.debry@free.fr>
2017-06-05 15:25:17 +0200

    plot6 theme

--- plot6.R
+++ plot6.R
@@ -6,22 +6,21 @@ if(!exists("SCC")){
     SCC <- readRDS("./data/Source_Classification_Code.rds")
 }
 
-
-
 E_BAL_LA <- subset(NEI,(NEI$fips=="24510" | NEI$fips=="06037"  ) & NEI$type=="ON-ROAD" )
 Motor_BAL_LA <- aggregate(Emissions ~ year + fips, E_BAL_LA, sum)
 
 county <-Motor_BAL_LA[,2]
-county[county[]== "06037"] <- "Los Angels"
+county[county[]== "06037"] <- "Los Angeles"
 county[county[]== "24510"] <- "Baltimore"
 county <- as.data.frame(county)
 Motor_BAL_LA <- cbind(Motor_BAL_LA, county)
 
 
-#png('plot6.png')
+png('plot6.png')
 p <- ggplot(Motor_BAL_LA, aes(x=year , y=Emissions, col=county)) 
 p <- p + geom_point(size=2) + geom_line(size=1)
 p <- p + ylab("PM 2.5 Emissions") 
 p <- p + ggtitle("Motor Emissions in Baltimore/LA  from 1999 to 2008")
+p <- p + theme(plot.title = element_text(hjust=0.5, colour="red"))
 print(p)
-#dev.off()
+dev.off()
